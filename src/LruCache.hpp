fe1ab58c47438e9bbbe191fb5f964db3e502a2ba
cursey <cursey@live.com>
2022-01-25 12:37:14 -0800

    Simplify LruCache

--- src/LruCache.hpp
+++ src/LruCache.hpp
@@ -1,11 +1,11 @@
-#include <unordered_map>
-#include <tuple>
 #include <list>
-#include <optional>
+#include <tuple>
+#include <unordered_map>
 
 template <typename KeyT, typename ValueT> class LruCache {
 public:
-    LruCache(size_t max_size) : m_max_size{max_size} {}
+    LruCache(size_t max_size)
+        : m_max_size{max_size} {}
 
     void put(const KeyT& key, const ValueT& value) {
         auto it = m_cache.find(key);
@@ -26,21 +26,19 @@ public:
         }
     }
 
-    std::optional<std::reference_wrapper<const ValueT>> get(const KeyT& key) {
+    ValueT* get(const KeyT& key) {
         auto it = m_cache.find(key);
 
         if (it == m_cache.end()) {
-            return std::nullopt;
+            return nullptr;
         }
 
         m_lru.splice(m_lru.begin(), m_lru, it->second);
 
-        return std::cref(std::get<1>(*(it->second)));
+        return &std::get<1>(*(it->second));
     }
 
-    auto has(const KeyT& key) {
-        return m_cache.find(key) != m_cache.end();
-    }
+    auto has(const KeyT& key) { return m_cache.find(key) != m_cache.end(); }
 
     auto size() const { return m_cache.size(); }
 
