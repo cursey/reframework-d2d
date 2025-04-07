8c0b3738f2050d2abf1be2f74688dec114456715
pblivingston <71585805+pblivingston@users.noreply.github.com>
2025-04-06 23:01:25 -0400

    Image alpha support (#22)
    Co-authored-by: cursey <cursey@users.noreply.github.com>

--- src/DrawList.hpp
+++ src/DrawList.hpp
@@ -90,6 +90,7 @@ public:
                 float y{};
                 float w{};
                 float h{};
+				float alpha{1.0f};
             } image;
             struct {
                 float x{};
@@ -164,7 +165,7 @@ public:
         void quad(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float thickness, unsigned int color);
         void fill_quad(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, unsigned int color);
         void line(float x1, float y1, float x2, float y2, float thickness, unsigned int color);
-        void image(std::shared_ptr<D2DImage>& image, float x, float y, float w, float h);
+        void image(std::shared_ptr<D2DImage>& image, float x, float y, float w, float h, float alpha = 1.0f);
         void fill_circle(float x, float y, float radiusX, float radiusY, unsigned int color);
         void circle(float x, float y, float radiusX, float radiusY, float thickness, unsigned int color);
         void pie(float x, float y, float r, float startAngle, float sweepAngle, unsigned int color, bool clockwise);
